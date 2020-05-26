61c696568bbf8f19a67a205759a2db67f24f54e0
Tuxzx <tuxzx@outlook.com>
2020-05-26 15:33:16 +0800

    fix person table: add col

--- profile_provider/models.py
+++ profile_provider/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=30)
     introduction = models.TextField()
+    background = models.TextField()
     looks = models.TextField()
     character = models.TextField()
     ability = models.TextField()
