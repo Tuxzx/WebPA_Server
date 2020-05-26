9485183c9d0a413b3806088b79d8d9125ebba3c7
Tuxzx <tuxzx@outlook.com>
2020-05-26 22:36:35 +0800

    fix: json encode not is utf-8

--- profile_provider/views.py
+++ profile_provider/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Person
+import json
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world.")
+
+def getPersonAll(request):
+    all_entries = Person.objects.all()
+    result = dict()
+    for person in all_entries:
+        temp = dict()
+        temp['introduction']=person.introduction
+        temp['background']=person.background
+        temp['looks']=person.looks
+        temp['character']=person.character
+        temp['ability']=person.ability
+        temp['weakness']=person.weakness
+        temp['extend']=person.extend
+        result[str(person.name)] = temp
+    return HttpResponse(json.dumps(result, ensure_ascii=False), content_type='application/json; charset=utf-8')
--- serverside/settings.py
+++ serverside/settings.py
@@ -38,17 +38,22 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profile_provider.apps.ProfileProviderConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'serverside.urls'
 
@@ -104,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -119,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+DEFAULT_CHARSET='utf-8'
